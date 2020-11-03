1495df41bb9291cc212f93b279a4a133eb0324ae
nouara <nedprofit@gmail.com>
2020-11-03 10:42:51 +0300

    added a method of generating promocodes

--- app/models/promocode.rb
+++ app/models/promocode.rb
@@ -1,2 +1,13 @@
 class Promocode < ApplicationRecord
+  def generate_promo(str)
+    str = str.chars.map do |c|
+      if c == '@' then c = (Array('A'..'Z') + Array('a'..'z')).sample
+      elsif c == '#' then c = SecureRandom.random_number(10)
+      elsif c == '*' then c = (Array('A'..'Z') + Array('a'..'z') + Array('0'..'9')).sample
+      end
+      c
+    end
+    str.join
+  end
+
 end
